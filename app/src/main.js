d2dfa07d106ea230829e3ce18d9251696bb0d705
Devric <billbox13@gmail.com>
2014-06-19 21:00:11 +1000

    add modifier to transform positioning

--- app/src/main.js
+++ app/src/main.js
@@ -2,25 +2,26 @@
 define(function(require, exports, module) {
     'use strict';
     // import dependencies
-    var Engine = require('famous/core/Engine');
-    var Modifier = require('famous/core/Modifier');
-    var Transform = require('famous/core/Transform');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
-    var Surface = require('famous/core/Surface')
+    var Engine        = require('famous/core/Engine');
+    var Modifier      = require('famous/core/Modifier');
+    var Transform     = require('famous/core/Transform');
+    var ImageSurface  = require('famous/surfaces/ImageSurface');
+    var Surface       = require('famous/core/Surface')
+    var StateModifier = require('famous/modifiers/StateModifier')
+    
 
     var mainContext = Engine.createContext()
-    var firstSurface = new Surface({
-        size : [200, 400]
-      , content : 'hello world'
-      , properties : {
-            color           : 'white'
-          , textAlign       : 'center'
-          , backgroundColor : '#FA5C4F'
-          , borderRadius    : '15px'
-          , fontSize        : '40px'
-      }
+
+    var stateModifier = new StateModifier({
+        transform: Transform.translate(150,100,0)
     })
 
-    mainContext.add(firstSurface)
+    var firstSurface = new Surface({
+        size : [200, 200]
+      , properties : {
+          backgroundColor : '#FA5C4F'
+        }
+    })
 
+    mainContext.add(stateModifier).add(firstSurface)
 });
