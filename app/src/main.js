9a0c960bcdc8e897210dac0f4a489370224d1e92
Devric <billbox13@gmail.com>
2014-06-19 20:45:39 +1000

    add hellow world

--- app/src/main.js
+++ app/src/main.js
@@ -6,24 +6,13 @@ define(function(require, exports, module) {
     var Modifier = require('famous/core/Modifier');
     var Transform = require('famous/core/Transform');
     var ImageSurface = require('famous/surfaces/ImageSurface');
+    var Surface = require('famous/core/Surface')
 
-    // create the main context
-    var mainContext = Engine.createContext();
+    var mainContext = Engine.createContext()
+    var firstSurface = new Surface({
+        content : 'hello world'
+    })
 
-    // your app here
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: '/content/images/famous_logo.png',
-        classes: ['backfaceVisibility']
-    });
+    mainContext.add(firstSurface)
 
-    var initialTime = Date.now();
-    var centerSpinModifier = new Modifier({
-        origin: [0.5, 0.5],
-        transform : function() {
-            return Transform.rotateY(.002 * (Date.now() - initialTime));
-        }
-    });
-
-    mainContext.add(centerSpinModifier).add(logo);
 });
