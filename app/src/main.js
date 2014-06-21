b6a62a611cc3886c5e600408456905176077d507
Devric <billbox13@gmail.com>
2014-06-21 23:49:11 +1000

    add custom effects

--- app/src/main.js
+++ app/src/main.js
@@ -2,14 +2,18 @@
 define(function(require, exports, module) {
     'use strict';
     // import dependencies
-    var Engine        = require('famous/core/Engine');
-    var Modifier      = require('famous/core/Modifier');
-    var Transform     = require('famous/core/Transform');
-    var ImageSurface  = require('famous/surfaces/ImageSurface');
-    var Surface       = require('famous/core/Surface')
-    var StateModifier = require('famous/modifiers/StateModifier')
-    var View          = require('famous/core/View')
-    var Easing        = require('famous/transitions/Easing')
+    var Engine           = require('famous/core/Engine');
+    var Modifier         = require('famous/core/Modifier');
+    var Transform        = require('famous/core/Transform');
+    var ImageSurface     = require('famous/surfaces/ImageSurface');
+    var Surface          = require('famous/core/Surface')
+    var StateModifier    = require('famous/modifiers/StateModifier')
+    var View             = require('famous/core/View')
+    var Easing           = require('famous/transitions/Easing')
+    var SpringTransition = require('famous/transitions/SpringTransition')
+    var Transitionable   = require('famous/transitions/Transitionable')
+    Transitionable.registerMethod('spring', SpringTransition)
+    
 
     var mainContext = Engine.createContext()
     var view = new View()
@@ -28,22 +32,14 @@ define(function(require, exports, module) {
     })
     mainContext.add(mod).add(surface)
 
-    // now we transition it
+    var spring = {
+        method: 'spring',
+        period: 1000,
+        dampingRatio: 0.3
+    }
+
     mod.setTransform(
-            Transform.translate(0,300,0),
-            {duration:8000,curve:'linear'}
+        Transform.translate(0,400,0), spring
     )
-
-    surface.on('click', function(){
-        // halting on the first animation
-        mod.halt()
-        surface.setContent('halted')
-
-        // but this new animation will carry out immediately
-        mod.setTransform(
-            Transform.translate(0,400,0),
-            {duration:400,curve:Easing.outBounce}
-        )
-    })
-
 });
+
