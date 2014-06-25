6344bca6346458110266399c5d4dc1211dcc0fd7
Devric <billbox13@gmail.com>
2014-06-25 20:47:56 +1000

    add lightbox

--- app/src/views/SlideshowView.js
+++ app/src/views/SlideshowView.js
@@ -6,19 +6,37 @@ define(function(require,exports, module){
     var Surface = require('famous/core/Surface')
     var Transform = require('famous/core/Transform')
     var StateModifier = require('famous/modifiers/StateModifier')
+    var Lightbox = require('famous/views/Lightbox')
 
     var SlideView = require('views/SlideView')
+
+    function _createLightbox() {
+        this.lightbox = new Lightbox(this.options.lightboxOpts)
+        this.mainNode.add(this.lightbox)
+    }
 
     function SlideshowView() {
         View.apply(this, arguments)
         var slideView = new SlideView()
-        this.add(slideView)
+
+        this.rootMod = new StateModifier({
+            size   : this.options.size
+          , origin : [0.5,0]
+          , align  : [0.5,0 ]
+        })
+
+        this.mainNode = this.add(slideView)
+        _createLightbox.call(this)
     }
 
     SlideshowView.prototype = Object.create(View.prototype)
     SlideshowView.prototype.constructor = SlideshowView
 
-    SlideshowView.DEFAULT_OPTIONS = {}
+    SlideshowView.DEFAULT_OPTIONS = {
+        size         : [450,500]
+      , lightboxOpts : {}
+    }
+
     module.exports = SlideshowView
 
 })
