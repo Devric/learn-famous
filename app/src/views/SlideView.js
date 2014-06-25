28e9ace7a53b2da5602f926f60ec5ed8f45ba663
Devric <billbox13@gmail.com>
2014-06-25 20:42:54 +1000

    Add slideView

--- app/src/views/SlideView.js
+++ app/src/views/SlideView.js
@@ -4,20 +4,28 @@ define(function(require,exports, module){
 
     var View = require('famous/core/View')
     var Surface = require('famous/core/Surface')
+    var ImageSurface = require('famous/surfaces/ImageSurface')
+    
     var Transform = require('famous/core/Transform')
     var StateModifier = require('famous/modifiers/StateModifier')
-
+    
+    // var SlideData = require('SlideData')
+    SlideData = {}
+    SlideData.defaultImage = 'a.jpg'
+    
     function SlideView() {
         View.apply(this, arguments)
 
         this.rootModifier = new StateModifier({
             size: this.options.size
+          , origin: [0.5,0]
         })
 
         this.mainNode = this.add(this.rootModifier)
 
         _createBackground.call(this)
         _createFilm.call(this)
+        _createPhoto.call(this)
 
     }
 
@@ -27,6 +35,8 @@ define(function(require,exports, module){
     SlideView.DEFAULT_OPTIONS = {
         size       : [400,500]
       , filmBorder : 15
+      , photoBorder : 3
+      , photoUrl : SlideData.defaultImage
     }
     module.exports = SlideView
 
@@ -62,5 +72,23 @@ define(function(require,exports, module){
         this.mainNode.add(filmMod).add(film)
     }
 
+    function _createPhoto() {
+        var photoSize = this.options.filmSize - 2 * this.options.photoBorder
+        var photo = new ImageSurface({
+            size : [photoSize, photoSize]
+          , content : 'this.options.photoUrl'
+          , properties: {
+                zIndex: 2
+            }
+        })
+
+        this.photoMod = new StateModifier({
+            origin: [0.5,0]
+          , align: [0.5,0]
+          , transform: Transform.translate(0,this.options.filmBorder,2)
+        })
+
+        this.mainNode.add(this.photoMod).add(photo)
+    }
 
 })
